bcc2e69e814af09a41e144ec48160846fb5a88fe
Maxomys <mistrz7losos@live.com>
2024-07-29 23:55:03 +0200

    fix and add integration tests

--- src/test/java/com/github/maxomys/repofetcher/integration/RepositoryRestControllerIT.java
+++ src/test/java/com/github/maxomys/repofetcher/integration/RepositoryRestControllerIT.java
@@ -1,5 +1,9 @@
 package com.github.maxomys.repofetcher.integration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,7 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import com.github.maxomys.repofetcher.api.RepositoryResponse;
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 
@@ -16,7 +21,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @WireMockTest()
-@AutoConfigureWebTestClient(timeout = "9999999")
+@AutoConfigureWebTestClient(timeout = "15000")
 public class RepositoryRestControllerIT {
 
   @Autowired
@@ -38,31 +43,63 @@ public class RepositoryRestControllerIT {
   void getNonForkRepositoriesForUsernameTest() {
     // repos
     stubFor(get(urlPathTemplate("/users/{username}/repos"))
-        .withQueryParam("page", matching("^[1-2]$"))
         .willReturn(aResponse()
             .withStatus(200)
             .withHeader("Content-Type", "application/json")
             .withBodyFile("ghUserReposResponse.json")));
 
-    // empty page
-    stubFor(get(urlPathTemplate("/users/{username}/repos"))
-        .withQueryParam("page", equalTo("3"))
-        .willReturn(aResponse()
-            .withStatus(200)
-            .withHeader("Content-Type", "application/json")
-            .withBody("[]")));
-
     // branches
     stubFor(get(urlPathTemplate("/repos/{username}/{repo}/branches"))
-        .withQueryParam("page", matching("^[1-2]$"))
         .willReturn(aResponse()
             .withStatus(200)
             .withHeader("Content-Type", "application/json")
             .withBodyFile("ghBranchesResponse.json")));
 
-    // empty page
+    webTestClient
+        .get()
+        .uri("/api/repos/foo")
+        .exchange()
+        .expectStatus()
+        .is2xxSuccessful()
+        .expectBodyList(RepositoryResponse.class).value(repositories -> {
+          assertEquals(3, repositories.size());
+          assertFalse(repositories.get(0).repositoryName().isEmpty());
+          assertEquals(3, repositories.get(1).branches().size());
+        });
+  }
+
+  @Test
+  void getNonForkRepositoriesForUsernameAllForksTest() {
+    stubFor(get(urlPathTemplate("/users/{username}/repos"))
+        .willReturn(aResponse()
+            .withStatus(200)
+            .withHeader("Content-Type", "application/json")
+            .withBodyFile("ghUserReposResponseAllForks.json")));
+
+    webTestClient
+        .get()
+        .uri("/api/repos/foo")
+        .exchange()
+        .expectStatus()
+        .is2xxSuccessful()
+        .expectBodyList(RepositoryResponse.class).value(repositories -> {
+          assertTrue(repositories.isEmpty());
+        });
+
+    verify(exactly(0), getRequestedFor(urlPathTemplate("/repos/{username}/{repo}/branches")));
+  }
+
+  @Test
+  void getNonForkRepositoriesForUsernameNoBranchesTest() {
+    // repos
+    stubFor(get(urlPathTemplate("/users/{username}/repos"))
+        .willReturn(aResponse()
+            .withStatus(200)
+            .withHeader("Content-Type", "application/json")
+            .withBodyFile("ghUserReposResponse.json")));
+
+    // empty list of branches
     stubFor(get(urlPathTemplate("/repos/{username}/{repo}/branches"))
-        .withQueryParam("page", equalTo("3"))
         .willReturn(aResponse()
             .withStatus(200)
             .withHeader("Content-Type", "application/json")
@@ -74,9 +111,10 @@ public class RepositoryRestControllerIT {
         .exchange()
         .expectStatus()
         .is2xxSuccessful()
-        .expectBody()
-        .jsonPath("$[0].branches[0]")
-        .isNotEmpty();
+        .expectBodyList(RepositoryResponse.class).value(repositories -> {
+          assertEquals(3, repositories.size());
+          assertTrue(repositories.get(0).branches().isEmpty());
+        });
   }
 
   @Test
@@ -101,7 +139,9 @@ public class RepositoryRestControllerIT {
         .is4xxClientError()
         .expectBody()
         .jsonPath("$.status")
-        .isEqualTo(404);
+        .isEqualTo(404)
+        .jsonPath("$.message")
+        .isEqualTo("The user you are looking for does not exist.");
   }
 
 }
